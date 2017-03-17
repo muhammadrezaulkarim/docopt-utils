66f3accfec3fc3aca3917523a90c045b76ea60cf
King Chung Huang <kinghuang@mac.com>
2017-03-17 14:28:45 -0600

    Add parse function for command handler and opts
    Implement a parse function that determines the command handler function
    and its options from the command arguments. This adapted from
    docopt_command in converis-stream, which was originally derived from
    Docker Compose.

---
+++ docopt_utils/dispatcher.py
@@ -0,0 +1,52 @@
+from inspect import getdoc
+
+from docopt import docopt
+from docopt import DocoptExit
+
+
+def parse(command_classes, command='__root__', command_opts=None, docopt_opts={}, args=None):
+    command_class = command_classes.get(command)
+    if not command_class:
+        raise Exception()
+    command_help = getdoc(command_class)
+    if not command_opts:
+        command_opts = {}
+    command_opts.update(_docopt(command_help, args, **docopt_opts))
+    command = command_opts['COMMAND']
+
+    if command in command_classes:
+        return parse(command_classes, command=command, command_opts=command_opts,
+                     docopt_opts=docopt_opts, args=[command] + options['ARGS'])
+
+    if not command or command in ('-h', '--help'):
+        raise SystemExit(command_help)
+
+    command_handler = get_handler(command_class, command)
+    command_help = getdoc(handler)
+    if command_help is None:
+        raise NoSuchCommand(command)
+
+    command_opts.update(_docopt(command_help, command_opts['ARGS'], options_first=True))
+    return handler, command_opts
+
+
+def _docopt(docstring, *agrs, **kwargs):
+    try:
+        return docopt(docstring, *args, **kwargs)
+    except DocoptExit:
+        raise SystemExit(docstring)
+
+
+def get_handler(command_class, command):
+    command_name = command.replace('-', '_')
+    if not hasattr(command_class, command_name):
+        raise NoSuchCommand(command, command_class)
+    instance = command_class()
+    return getattr(instance, command)
+
+
+class NoSuchCommand(Exception):
+    def __init__(self, command, container):
+        super().__init__(f'No such command: {command}')
+        self.command = command
+        self.container = container
